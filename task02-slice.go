570da78ee8eba23b6ea850ab9e60bf9e0677f87f
Demid <dagdog2506@gmail.com>
2022-05-06 15:44:17 +0500

    Homework 3 done.

--- task02-slice.go
+++ task02-slice.go
@@ -2,5 +2,12 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
-	return
+	result = make([]int64, len(input))
+
+	copy(result, input)
+
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
+	}
+	return result
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,25 @@
 package homework
 
+import (
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
-	return
+
+	m := map[int]string{2: "test1", 1: "test2", 4: "test3"}
+
+	keys := make([]int, 0, len(input))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	
+	sort.Ints(keys)
+
+	values := make([]string,0,len(input))
+	for _, k := range keys {
+		values = append(values, input[k])
+	}
+
+	return values
 }
